110a51371f34a973d8bde4dcb7d6dc0e94b38ca9
cjheppell <chris.john.heppell@gmail.com>
2021-02-11 17:28:08 +0000

    rename env vars in create image and check they exist

--- test/create-images.sh
+++ test/create-images.sh
@@ -8,12 +8,27 @@ function logSpawnMessage() {
     printf "🛸  ${GREEN}$1${NC}\n"
 }
 
+if [[ -z $ACCOUNT_TEST_SPAWN_DATA_IMAGE_NAME ]]; then
+    logSpawnMessage "Error: you must specify a name for the account spawn data image by setting the 'ACCOUNT_TEST_SPAWN_DATA_IMAGE_NAME' environment variable"
+    exit 1
+fi
+
+if [[ -z $TODO_TEST_SPAWN_DATA_IMAGE_NAME ]]; then
+    logSpawnMessage "Error: you must specify a name for the todo spawn data image by setting the 'TODO_TEST_SPAWN_DATA_IMAGE_NAME' environment variable"
+    exit 1
+fi
+
+if [[ -z $TAG ]]; then
+    logSpawnMessage "Error: you must specify a tag to use for tagging the created data images by setting the 'TAG' environment variable"
+    exit 1
+fi
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-logSpawnMessage "Creating 'Todo' Spawn data image for testing with name: '${SPAWN_TODO_IMAGE_NAME}:${TAG}'"
-spawnctl create data-image -f ./database/todo/spawn/test.yaml --name $SPAWN_TODO_IMAGE_NAME --tag $TAG -q > /dev/null
-logSpawnMessage "Successfully created Spawn data image '${SPAWN_TODO_IMAGE_NAME}:${TAG}'"
+logSpawnMessage "Creating 'Todo' Spawn data image for testing with name: '${TODO_TEST_SPAWN_DATA_IMAGE_NAME}:${TAG}'"
+spawnctl create data-image -f ./database/todo/spawn/test.yaml --name $TODO_TEST_SPAWN_DATA_IMAGE_NAME --tag $TAG -q > /dev/null
+logSpawnMessage "Successfully created Spawn data image '${TODO_TEST_SPAWN_DATA_IMAGE_NAME}:${TAG}'"
 
-logSpawnMessage "Creating 'Account' Spawn data image for testing with name: '${SPAWN_ACCOUNT_IMAGE_NAME}:${TAG}'"
-spawnctl create data-image -f ./database/account/spawn/test.yaml --name $SPAWN_ACCOUNT_IMAGE_NAME --tag $TAG -q > /dev/null
-logSpawnMessage "Successfully created Spawn data image '${SPAWN_ACCOUNT_IMAGE_NAME}:${TAG}'"
+logSpawnMessage "Creating 'Account' Spawn data image for testing with name: '${ACCOUNT_TEST_SPAWN_DATA_IMAGE_NAME}:${TAG}'"
+spawnctl create data-image -f ./database/account/spawn/test.yaml --name $ACCOUNT_TEST_SPAWN_DATA_IMAGE_NAME --tag $TAG -q > /dev/null
+logSpawnMessage "Successfully created Spawn data image '${ACCOUNT_TEST_SPAWN_DATA_IMAGE_NAME}:${TAG}'"
