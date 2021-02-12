b83dd7feffffab5d46f9c5e4c5287669811b4590
cjheppell <chris.john.heppell@gmail.com>
2021-02-12 14:10:52 +0000

    trust the sql server certificate
    Spawn currently uses a self-signed cert, so this is necessary

--- api/Spawn.Demo.Store/AccountConnectionService.cs
+++ api/Spawn.Demo.Store/AccountConnectionService.cs
@@ -1,12 +1,25 @@
+using System.Data.SqlClient;
+
 namespace Spawn.Demo.Store
 {
     public class AccountConnectionService
     {
+        private readonly string _connectionString;
+
         public AccountConnectionService(string connString)
         {
-            ConnString = connString;
+            _connectionString = connString;
         }
 
-        public string ConnString { get; }
+        public string ConnString {
+            get
+            {
+                var sqlConnectionStringBuilder = new SqlConnectionStringBuilder(_connectionString)
+                {
+                    TrustServerCertificate = true
+                };
+                return sqlConnectionStringBuilder.ToString();
+            }
+        }
     }
 }
--- api/Spawn.Demo.Store/TodoConnectionService.cs
+++ api/Spawn.Demo.Store/TodoConnectionService.cs
@@ -1,12 +1,26 @@
+using Npgsql;
+
 namespace Spawn.Demo.Store
 {
     public class TodoConnectionService
     {
+        private readonly string _connString;
+        
         public TodoConnectionService(string connString)
         {
-            ConnString = connString;
+            _connString = connString;
         }
 
-        public string ConnString { get; }
+        public string ConnString
+        {
+            get
+            {
+                var pgsqlConnectionStringBuilder = new NpgsqlConnectionStringBuilder(_connString)
+                {
+                    TrustServerCertificate = true,
+                };
+                return pgsqlConnectionStringBuilder.ToString();
+            }
+        }
     }
 }
