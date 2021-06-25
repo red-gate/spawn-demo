44aeeed0c0920b422d8fba89018a17d87ccc19bc
cjheppell <chris.john.heppell@gmail.com>
2021-06-25 12:00:20 +0100

    add a script for startup of the gitpod env

---
+++ gitpod-start.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo 'You must authenticate to the Spawn service to run this example app'
+echo ''
+echo 'Open the URL presented below and follow the instructions to authenticate'
+echo ''
+spawnctl auth
+
+source .env
+source spawn.sh
+
+validateImagesExist
+setupContainers
+migrateDatabases
+
+echo ''
+echo 'Building .NET app...'
+
+dotnet build ./api/Spawn.Demo.WebApi
+
+echo ''
+echo 'Restoring frontend dependencies...'
+
+pushd ./client
+yarn
+popd
+
+echo 'Environment set up successfully!'
+echo 'In two separate terminals run:
+
+  dotnet run --project ./api/Spawn.Demo.WebApi
+
+  cd client && yarn start
+  
+'
