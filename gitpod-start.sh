5fa8a8310e65f46a0ff3aa61cc6f8c87ba65f5e9
cjheppell <chris.john.heppell@gmail.com>
2021-06-29 11:01:48 +0100

    handle errors on spawnctl auth

--- gitpod-start.sh
+++ gitpod-start.sh
@@ -4,7 +4,18 @@ echo 'You must authenticate to the Spawn service to run this example app'
 echo ''
 echo 'Open the URL presented below and follow the instructions to authenticate'
 echo ''
-spawnctl auth
+if spawnctl auth; then 
+  RED='\033[0;31m'
+  NC='\033[0m' # No Color
+  echo ''
+  echo -e "${RED}Error authenticating to Spawn.${NC}"
+  echo ''
+  echo -e "${RED}Please ensure you've already signed up to Spawn by visiting https://app.spawn.cc/login${NC}"
+  echo ''
+  echo -e "${RED}Then re-run ./gitpod-start.sh${NC}"
+  exit 1
+fi
+
 
 source .env
 source spawn.sh
