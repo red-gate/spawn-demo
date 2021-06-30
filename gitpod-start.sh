8882b8464bc0bbe6bc00d4fdbfefd2259d2c835c
cjheppell <chris.john.heppell@gmail.com>
2021-06-30 15:59:54 +0100

    prompt for cli based onboarding on auth failures in gitpod
    rather than making users jump out to the website

--- gitpod-start.sh
+++ gitpod-start.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-echo 'You must authenticate to the Spawn service to run this example app'
-echo ''
-echo 'Open the URL presented below and follow the instructions to authenticate'
-echo ''
-if ! spawnctl auth || ! spawnctl get data-images > /dev/null 2>&1 ; then
+if ! spawnctl get data-images > /dev/null 2>&1 ; then
   RED='\033[0;31m'
   NC='\033[0m' # No Color
+  echo -e "${RED}You must authenticate to the Spawn service to run this example app${NC}"
   echo ''
-  echo -e "${RED}Error authenticating to Spawn.${NC}"
+  echo -e "${RED}Open the URL presented below and follow the instructions to authenticate${NC}"
   echo ''
-  echo -e "${RED}Please ensure you've already signed up to Spawn by visiting https://app.spawn.cc/login${NC}"
-  echo ''
-  echo -e "${RED}Then re-run ./gitpod-start.sh${NC}"
-  exit 1
+  if ! spawnctl auth || ! spawnctl get data-images > /dev/null 2>&1 ; then
+    echo ''
+    echo -e "${RED}Error authenticating to Spawn.${NC}"
+    echo ''
+    echo -e "${RED}Please ensure you've already signed up to Spawn by running 'spawnctl onboard'${NC}"
+    echo ''
+    echo -e "${RED}Then re-run ./gitpod-start.sh${NC}"
+    exit 1
+  fi
 fi
-
 
 source .env
 source spawn.sh
